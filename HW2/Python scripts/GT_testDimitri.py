e61120acc25a95fe0a6c4f8336436771e43e2837
DimitriBot <91071728+DimitriBot@users.noreply.github.com>
2021-10-09 17:38:14 +0200

    Update GT_testDimitri.py

--- HW2/Python scripts/GT_testDimitri.py
+++ HW2/Python scripts/GT_testDimitri.py
@@ -244,17 +244,17 @@ def gas_turbine(P_e,options,display):
     
     flow_air = P_e/(eta_mec*((1+(1/lamb_ma1))*(h_3 - h_4) - (h_2 - h_1)));  #Air mass flow rate [kg_air/s]
     
-    loss_mec = k_mec*((1+(1/lamb_ma1))*(h_3 - h_4) + (h_2 - h_1))*flow_air; #Mechanical losses [kW]
-    loss_ech = ((1+(1/lamb_ma1))*h_4 - h_1)*flow_air;                       #Exhaust gas losses [kW]
+    loss_mec = k_mec*((1+(1/lamb_ma1))*(h_3 - h_4) + (h_2 - h_1))*flow_air; #Mechanical losses [W]
+    loss_ech = ((1+(1/lamb_ma1))*h_4 - h_1)*flow_air;                       #Exhaust gas losses [W]
     
     ## Exergetic losses
     # =================
     
-    loss_combex = flow_air*(e_2 + f*LHV/lamb_ma1 - (1+(1/lamb_ma1))*e_3); #Combustion losses [kW]
-    loss_turbine = flow_air*(1+(1/lamb_ma1))*((e_3 - e_4) - (h_3 - h_4)); #Shaft losses at the turbine [kW]
-    loss_compressor = flow_air*((h_2 - h_1) - (e_2 - e_1));               #Shaft losses at the compressor [kW]
-    loss_rotex = (loss_compressor + loss_turbine);                        #Total shaft losses [kW]
-    loss_echex = flow_air*((1+(1/lamb_ma1))*e_4 - e_1);                   #Exhaust gas losses [kW]
+    loss_combex = flow_air*(e_2 + f*LHV/lamb_ma1 - (1+(1/lamb_ma1))*e_3); #Combustion losses [W]
+    loss_turbine = flow_air*(1+(1/lamb_ma1))*((e_3 - e_4) - (h_3 - h_4)); #Shaft losses at the turbine [W]
+    loss_compressor = flow_air*((h_2 - h_1) - (e_2 - e_1));               #Shaft losses at the compressor [W]
+    loss_rotex = (loss_compressor + loss_turbine);                        #Total shaft losses [W]
+    loss_echex = flow_air*((1+(1/lamb_ma1))*e_4 - e_1);                   #Exhaust gas losses [W]
     
     ## Mass flows
     # ===========
@@ -262,6 +262,30 @@ def gas_turbine(P_e,options,display):
     dotm_a = flow_air; #Air mass flow rate [kg_air/s]
     dotm_f = flow_air/lamb_ma1; #Combustible mass flow rate [kg_comb/s]
     dotm_g = (1+(1/lamb_ma1))*flow_air;#Fluegas mass flow rate [kg_fluegas/s]
+    
+    ## Generate graph to export:
+    # ==========================
+   
+    # My 1st figure: Pie chart energy
+    fig_pie_en = plt.figure(1)
+    labels = 'Effective Power \n'+ '%.1f'%(Pe*1e-6)+' MW', 'Mechanical losses \n'+'%.1f'%(loss_mec*1e-6)+' MW', 'Exhaust losses MW \n'+'%.1f'%(loss_ech*1e-6)+' MW'
+    sizes = [Pe*1e-6, loss_mec*1e-6, loss_ech*1e-6]
+    colors = ['gold', 'yellowgreen', 'lightcoral']
+    #explode = (0, 0, 0, 0)
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=140)
+    plt.axis('equal')
+    plt.title("Primary energy flux " + "%.1f" %(LHV*dotm_f*1e-6) + " MW")
+   
+    # My 2nd figure: Pie chart exergy
+    fig_pie_ex = plt.figure(2)
+    labels = 'Effective Power \n'+ '%.1f'%(Pe*1e-6)+' MW', 'Mechanical losses \n'+'%.1f'%(loss_mec*1e-6)+' MW', 'Exhaust losses \n'+'%.1f'%(loss_echex*1e-6)+' MW', 'Turbine & compressor \n irreversibilities \n'+'%.1f'%(loss_rotex*1e-6)+' MW', 'Combustion \n irreversibilities \n'+'%.1f'%(loss_combex*1e-6)+' MW'
+    sizes = [Pe*1e-6, loss_mec*1e-6, loss_echex*1e-6, loss_rotex*1e-6, loss_combex*1e-6]
+    colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'red']
+    #explode = (0, 0, 0, 0)
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=340)
+    plt.axis('equal')
+    plt.title("Primary exergy flux " + "%.1f" %(e_c*dotm_f*1e-6) + " MW")
+    
     
     
     # Process output variables - do not modify---------------------------------
